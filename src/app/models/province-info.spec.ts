5b80c95e6760d34177736c5628ce5998cc65f1d7
Momfus Arboleo <momfusutn@gmail.com>
2019-04-21 04:08:36 -0300

    Arreglado los test unitarios con valores por defecto de los models

--- src/app/models/province-info.spec.ts
+++ src/app/models/province-info.spec.ts
@@ -2,6 +2,16 @@ import { ProvinceInfo } from './province-info';
 
 describe('ProvinceInfo', () => {
   it('should create an instance', () => {
-    expect(new ProvinceInfo()).toBeTruthy();
+
+    const auxId = 3844421;
+    const auxName = 'Mendoza';
+    const auxCountry = 'Ar';
+    const auxCoord = {
+      lon: -68.8272,
+      lat: -32.8909
+    };
+
+    expect(  new ProvinceInfo(auxId, auxName, auxCountry, auxCoord) ).toBeTruthy();
+
   });
-});
+})
--- src/app/models/weather-info.spec.ts
+++ src/app/models/weather-info.spec.ts
@@ -1,7 +1,17 @@
 import { WeatherInfo } from './weather-info';
 
 describe('WeatherInfo', () => {
+  
   it('should create an instance', () => {
-    expect(new WeatherInfo()).toBeTruthy();
+    
+    const auxTempMin = 18;
+    const auxTempMax = 22;
+    const auxHumidity = 30;
+    const auxWeater = 'Clear';
+    const auxDate = new Date();
+    const auxIcon = '01n';
+
+
+    expect( new WeatherInfo( auxTempMin, auxTempMax, auxHumidity, auxWeater, auxDate, auxIcon ) ).toBeTruthy();
   });
 });
